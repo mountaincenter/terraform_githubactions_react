b69b7cc5643a6b78a68da4345951fe49e1ee0a78
mountaincenter <y.hiroyuki560730@gmail.com>
2023-03-15 18:04:22 +0900

    create

--- frontend/react-app/src/App.tsx
+++ frontend/react-app/src/App.tsx
@@ -10,6 +10,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edit Terraform GitHub Actions CI/CD</p>
         <p>{process.env.REACT_APP_NODE_ENV}</p>
+        {process.env.REACT_APP_NODE_ENV === 'development' ? (
+          <p>localhost:3000</p>
+        ) : (
+          <p>web.ymnk.fun</p>
+        )}
         <a
           className="App-link"
           href="https://reactjs.org"
